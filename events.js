42fd95377adc4cfd698f6450e98a992ccf928daf
andelar96@gmail.com <andelar96@gmail.com>
2017-06-17 01:45:22 +0200

    Remembered I had a JS standard to follow, general cleaning

--- events.js
+++ events.js
@@ -1,66 +1,70 @@
-// Stuffy values
-const period = 0.01;
+const period = 0.01
 
-var ticks = 25;
-var alarmOn = false;
+const maxTicks = 25
+var ticks = maxTicks
 
-function resetAlarm() {
-  chrome.alarms.clear('potato powers activate', function() {
-    ticks = 25;
+var alarmOn = false
 
+/*
+// Trying to get settings, doesn't seem to work yet
+function syncTicks () {
+  chrome.storage.sync.get({
+      sessionLength: 25
+    }, function (data) {
+      ticks = data.sessionLength
+    }
+  )
+}
+*/
+
+function startAlarm () {
+  chrome.alarms.create('timer', {
+    'periodInMinutes': period
+  })
+  chrome.browserAction.setBadgeText({
+    text: (ticks).toString()
+  })
+  chrome.browserAction.setIcon({
+    path: 'img/potato.svg'
+  })
+  alarmOn = true
+}
+
+function resetAlarm () {
+  ticks = maxTicks;
+  chrome.alarms.clear('timer', function () {
     chrome.browserAction.setBadgeText({
-      text: ""
-    });
-
-    chrome.browserAction.setIcon({
-      path: "img/potato_off.svg"
+      text: ''
     })
-
-    alarmOn = false;
+    chrome.browserAction.setIcon({
+      path: 'img/potato_off.svg'
+    })
+    alarmOn = false
   })
 }
 
-
-function toggleAlarm() {
-  // Make the alarm that will rock the world
+function toggleAlarm () {
   if (!alarmOn) {
-    chrome.alarms.create('potato powers activate', {
-      "periodInMinutes": period
-    });
-
-    chrome.browserAction.setBadgeText({
-      text: (ticks).toString()
-    });
-
-    chrome.browserAction.setIcon({
-      path: "img/potato.svg"
-    })
-    alarmOn = true;
+    startAlarm()
   }
   else {
-    resetAlarm();
+    resetAlarm()
   }
 }
 
-
-// When clicked, make alarms
-chrome.browserAction.onClicked.addListener(toggleAlarm);
-
-function handleTick() {
-  console.log(ticks);
+// Update badge, turn off alarm if needed
+function handleTick () {
+  console.log(ticks)
   chrome.browserAction.setBadgeText({
     text: (--ticks).toString()
-  });
-  if (ticks === 0){
-    resetAlarm();
+  })
+  if (ticks === 0) {
+    resetAlarm()
   }
 }
 
-// Basically on tick, maybe have sublisteners for end of ticks etc.
-chrome.alarms.onAlarm.addListener(handleTick);
+// When alarm ticks, handle it
+chrome.alarms.onAlarm.addListener(handleTick)
 
-// Try to clean up before reload/suspend?
-chrome.runtime.onSuspend.addListener(function() {
-  console.log("Trying to suspend");
-  resetAlarm();
-});
+// Turn on/off alarm
+chrome.browserAction.onClicked.addListener(toggleAlarm)
--- options.js
+++ options.js
@@ -1,32 +1,32 @@
-
 // Set options
-function save_options() {
-  var sessionLength = document.getElementById("sessionLength").value;
+function save_options () {
+  var sessionLength = document.getElementById('sessionLength').value
 
   chrome.storage.sync.set({
-    sessionLength: sessionLength
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById("status");
-    status.textContent = "Options saved.";
-    setTimeout(function() {
-      status.textContent = "";
-    }, 2000);
-  });
+      sessionLength: sessionLength
+    }, function () {
+      // Feedback for user
+      var status = document.getElementById('status')
+      status.textContent = 'Options saved.'
+      setTimeout(function () {
+        status.textContent = ''
+      }, 2000)
+    }
+  )
 }
 
 // Get stored options
-function restore_options() {
+function restore_options () {
 
   chrome.storage.sync.get({
     sessionLength: 25
-  }, function(data) {
-    document.getElementById("sessionLength").value = data.sessionLength;
-  });
+  }, function (data) {
+    document.getElementById('sessionLength').value = data.sessionLength
+  })
 }
 
 // Save options when clicking... Save.
-document.getElementById("save").addEventListener("click", save_options);
+document.getElementById('save').addEventListener('click', save_options)
 
 // Restore options when loading up the menu
-document.addEventListener("DOMContentLoaded", restore_options);
+document.addEventListener('DOMContentLoaded', restore_options)

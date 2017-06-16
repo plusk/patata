48e9325b9e96dca4b4720393f1c64b9135d94c25
andelar96@gmail.com <andelar96@gmail.com>
2017-06-17 00:28:38 +0200

    Implemented countdown that ends in a fizzle

--- events.js
+++ events.js
@@ -1,12 +1,12 @@
 // Stuffy values
 const period = 0.01;
 
-var ticks = 0;
+var ticks = 25;
 var alarmOn = false;
 
 function resetAlarm() {
   chrome.alarms.clear('potato powers activate', function() {
-    ticks = 0;
+    ticks = 25;
 
     chrome.browserAction.setBadgeText({
       text: ""
@@ -21,7 +21,7 @@ function resetAlarm() {
 }
 
 
-function toggleAlarm(clickInfo) {
+function toggleAlarm() {
   // Make the alarm that will rock the world
   if (!alarmOn) {
     chrome.alarms.create('potato powers activate', {
@@ -44,8 +44,11 @@ chrome.browserAction.onClicked.addListener(toggleAlarm);
 function handleTick() {
   console.log(ticks);
   chrome.browserAction.setBadgeText({
-    text: (++ticks).toString()
+    text: (ticks--).toString()
   });
+  if (ticks < 0){
+    resetAlarm();
+  }
 }
 
 // Basically on tick, maybe have sublisteners for end of ticks etc.

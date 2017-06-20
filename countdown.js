acc1a8c545b9ac6d0b0995d4211f942535228f51
andelar96@gmail.com <andelar96@gmail.com>
2017-06-20 02:48:48 +0200

    Added countdown timer that closes on end

--- countdown.js
+++ countdown.js
@@ -1,11 +1,41 @@
-var btn = document.getElementById('go')
+var docElement = document.documentElement
 
-function startBreak() {
-  btn.parentNode.removeChild(btn)
+var divCenter = document.getElementById('divCenter')
+var btnGo = document.getElementById('btnGo')
+var timer = document.createElement('p')
+
+
+function tickTimer (length) {
+  var min = parseInt(length / 60, 10)
+  var sec = parseInt(length % 60, 10)
+
+  min = min < 10 ? '0' + min : min
+  sec = sec < 10 ? '0' + sec : sec
+
+  return min + ':' + sec
 }
 
-btn.onclick = function () {
-  var docElement = document.documentElement
+function startBreak () {
+  divCenter.removeChild(btnGo)
+  divCenter.appendChild(timer)
+
+  var length = 5
+
+  timer.textContent = tickTimer(length)
+  length--
+
+  var tick = setInterval(function () {
+    timer.textContent = tickTimer(length)
+
+    if (--length < 0) {
+      clearTimeout(tick)
+      close()
+    }
+  }, 1000)
+
+}
+
+btnGo.onclick = function () {
 
   if (docElement.requestFullscreen) {
     docElement.requestFullscreen()
